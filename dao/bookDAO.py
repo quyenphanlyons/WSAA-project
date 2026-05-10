2362648dd0011eb88e1923d80b810608d54217a9
quyenphanlyons <quyenphanlyons@gmail.com>
2026-05-10 20:02:38 +0000

    add CRUD in bookDAO

--- dao/bookDAO.py
+++ dao/bookDAO.py
@@ -40,6 +40,142 @@ class BookDAO:
         # Close the connection
         connection.close()
 
+# CREATE: add a new book to the database
+    def create(self, book):
+        connection = sqlite3.connect(self.db)
+        cursor = connection.cursor()
+
+        sql = '''
+        INSERT INTO books (title, author, genre, rating, status, notes)
+        VALUES (?, ?, ?, ?, ?, ?)
+        '''
+
+        values = (
+            book['title'],
+            book['author'],
+            book['genre'],
+            book['rating'],
+            book['status'],
+            book['notes']
+        )
+
+        cursor.execute(sql, values)
+
+        book_id = cursor.lastrowid
+
+        connection.commit()
+        connection.close()
+
+        book['id'] = book_id
+
+        return book
+
+    # READ ALL: listall books from the database
+    def getAll(self):
+        connection = sqlite3.connect(self.db)
+        cursor = connection.cursor()
+
+        sql = 'SELECT * FROM books'
+
+        cursor.execute(sql)
+
+        results = cursor.fetchall()
+
+        connection.close()
+
+        books = []
+
+        for result in results:
+            book = {
+                'id': result[0],
+                'title': result[1],
+                'author': result[2],
+                'genre': result[3],
+                'rating': result[4],
+                'status': result[5],
+                'notes': result[6]
+            }
+
+            books.append(book)
+
+        return books
+
+    # READ ONE: find a specific book by its id
+    def findById(self, id):
+        connection = sqlite3.connect(self.db)
+        cursor = connection.cursor()
+
+        sql = 'SELECT * FROM books WHERE id = ?'
+
+        values = (id,)
+
+        cursor.execute(sql, values)
+
+        result = cursor.fetchone()
+
+        connection.close()
+
+        if result is None:
+            return None
+
+        book = {
+            'id': result[0],
+            'title': result[1],
+            'author': result[2],
+            'genre': result[3],
+            'rating': result[4],
+            'status': result[5],
+            'notes': result[6]
+        }
+
+        return book
+
+    # UPDATE: edit a book
+    def update(self, id, book):
+        connection = sqlite3.connect(self.db)
+        cursor = connection.cursor()
+
+        sql = '''
+        UPDATE books
+        SET title = ?,
+            author = ?,
+            genre = ?,
+            rating = ?,
+            status = ?,
+            notes = ?
+        WHERE id = ?
+        '''
+
+        values = (
+            book['title'],
+            book['author'],
+            book['genre'],
+            book['rating'],
+            book['status'],
+            book['notes'],
+            id
+        )
+
+        cursor.execute(sql, values)
+
+        connection.commit()
+        connection.close()
+
+    # DELETE: remove a book from the database
+    def delete(self, id):
+        connection = sqlite3.connect(self.db)
+        cursor = connection.cursor()
+
+        sql = 'DELETE FROM books WHERE id = ?'
+
+        values = (id,)
+
+        cursor.execute(sql, values)
+
+        connection.commit()
+        connection.close()
+
+
 # Create an instance of the BookDAO class
 # This allows other parts of the application to use it
 bookDAO = BookDAO()
