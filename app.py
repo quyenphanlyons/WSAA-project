653d537e5118835ad03e51ea7f1f975853537285
quyenphanlyons <quyenphanlyons@gmail.com>
2026-05-10 20:16:40 +0000

    add REST API to app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 from flask_cors import CORS
 from dao.bookDAO import bookDAO
 
@@ -16,6 +16,84 @@ bookDAO.createTable()
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+# REST API ROUTES
+
+# CREATE book
+@app.route('/books', methods=['POST'])
+def createBook():
+    if not request.json:
+        return jsonify({'message': 'Invalid request'}), 400
+
+    book = {
+        'title': request.json['title'],
+        'author': request.json['author'],
+        'genre': request.json.get('genre', ''),
+        'rating': request.json.get('rating', 0),
+        'status': request.json.get('status', 'Unread'),
+        'notes': request.json.get('notes', '')
+    }
+
+    createdBook = bookDAO.create(book)
+
+    return jsonify(createdBook), 201
+
+# GET all books
+@app.route('/books', methods=['GET'])
+def getAllBooks():
+    books = bookDAO.getAll()
+    return jsonify(books)
+
+
+# GET one book
+@app.route('/books/<int:id>', methods=['GET'])
+def findBookById(id):
+    book = bookDAO.findById(id)
+
+    if book is None:
+        return jsonify({'message': 'Book not found'}), 404
+
+    return jsonify(book)
+
+# UPDATE book
+@app.route('/books/<int:id>', methods=['PUT'])
+def updateBook(id):
+
+    foundBook = bookDAO.findById(id)
+
+    if foundBook is None:
+        return jsonify({'message': 'Book not found'}), 404
+
+    if not request.json:
+        return jsonify({'message': 'Invalid request'}), 400
+
+    updatedBook = {
+        'title': request.json['title'],
+        'author': request.json['author'],
+        'genre': request.json.get('genre', ''),
+        'rating': request.json.get('rating', 0),
+        'status': request.json.get('status', 'Unread'),
+        'notes': request.json.get('notes', '')
+    }
+
+    bookDAO.update(id, updatedBook)
+
+    return jsonify(updatedBook)
+
+# DELETE book
+@app.route('/books/<int:id>', methods=['DELETE'])
+def deleteBook(id):
+
+    foundBook = bookDAO.findById(id)
+
+    if foundBook is None:
+        return jsonify({'message': 'Book not found'}), 404
+
+    bookDAO.delete(id)
+
+    return jsonify({'message': 'Book deleted successfully'})
+
 
 if __name__ == '__main__':
     # Run the Flask development server with debug mode enabled
